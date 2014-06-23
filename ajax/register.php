3f549114cf1c8740b93b47f394b318b1b38878a8
kurtcam <kurtc@liquorice.co.za>
2014-06-23 17:00:18 +0000

    registration controller

--- ajax/register.php
+++ ajax/register.php
@@ -9,20 +9,24 @@ if (isset($_POST['full_name'])) {
         array('field_name'=>'dob',            'required'=>true,   'type'=>'date'),
         array('field_name'=>'email',          'required'=>true,   'type'=>'email'),
         array('field_name'=>'mobile',         'required'=>true,   'type'=>'numeric'),
-        array('field_name'=>'gender',         'required'=>true,   'type'=>'specific', 'defaults'=>array('m', 'f')),
-        array('field_name'=>'entry_type',     'required'=>true,   'type'=>'specific', 'defaults'=>array('entry', 'half_entry', 'full_entry', 'sponsor')),
-        array('field_name'=>'entry_location', 'required'=>true,   'type'=>'specific', 'defaults'=>array('pe', 'ct', 'not sure')),
-        array('field_name'=>'meals',          'required'=>false,  'type'=>'checkbox', 'defaults'=>array('sabbath', 'sunday', 'monday')),
-        array('field_name'=>'accomodation',   'required'=>false,  'type'=>'specific', 'defaults'=>array('yes', 'no')),
-        array('field_name'=>'payment',        'required'=>true,   'type'=>'specific', 'defaults'=>array('eft', 'deposit', 'cheque')),
-        array('field_name'=>'newsletter',     'required'=>false,  'type'=>'checkbox', 'defaults'=>array('monthly', 'conf_updates'))
+        array('field_name'=>'gender',         'required'=>true,   'type'=>'radio',     'defaults'=>array('m', 'f')),
+        array('field_name'=>'entry_type',     'required'=>true,   'type'=>'radio',     'defaults'=>array('entry', 'half_entry', 'full_entry', 'sponsor')),
+        array('field_name'=>'entry_location', 'required'=>true,   'type'=>'radio',     'defaults'=>array('pe', 'ct', 'not sure')),
+        array('field_name'=>'meals',          'required'=>false,  'type'=>'checkbox',  'defaults'=>array('sabbath', 'sunday', 'monday')),
+        array('field_name'=>'accomodation',   'required'=>false,  'type'=>'radio',     'defaults'=>array('yes', 'no')),
+        array('field_name'=>'payment',        'required'=>true,   'type'=>'radio',     'defaults'=>array('eft', 'deposit', 'cheque')),
+        array('field_name'=>'newsletters',    'required'=>false,  'type'=>'checkbox',  'defaults'=>array('monthly', 'conf_updates'))
     );
     $validate = new Validate($_POST, $fields);
-
-    if ($validate->valid()) {
-        $register = new Registration();
-        $register->createRegistration();
-        echo json_encode(array('success'=>'true'));
+    $cleaned_data = $validate->valid();
+    if ($cleaned_data) {
+        $register = new Registration($cleaned_data);
+        if ($register->createRegistration()){
+            echo json_encode(array('success'=>'true'));
+        }
+        else {
+            echo json_encode(array('success'=>'false'));
+        }
         exit();
     }
     header("HTTP/1.1 200 AMEN say no no!!", TRUE, 200);
--- controllers/registration.php
+++ controllers/registration.php
@@ -2,10 +2,68 @@
 
 class Registration {
     
-    function createRegistration() {
-        echo "Create registration";    
+    private $post_data;
+    private $db;
+    private $insert_query;
+    private $query_template;
+
+    function __construct($post_data) {
+       $this->post_data = $post_data;
+       $this->setInsertQuery();
+       $this->db = new mysqli("localhost", "root", "root", "amenconf");
+       if ($this->db->connect_errno) {
+           echo "An error has occurred. Our team has been notified and are investigating the problem. Thank you."; //$mysqli->connect_error;
+       }
         
     }    
+
+    function createRegistration() {
+       $res = $this->insertUserRegistration();
+       return $res;
+    }
+
+    private function setInsertQuery() {
+        $this->query_template = <<<EOD
+            INSERT INTO registration (
+                full_name, 
+                dob, 
+                email, 
+                mobile, 
+                gender, 
+                entry_type, 
+                entry_location, 
+                meals, 
+                accomodation, 
+                payment, 
+                newsletters, 
+                submission_date)
+            values (
+               '{$this->post_data['full_name']}',
+               '{$this->post_data['dob']}',
+               '{$this->post_data['email']}',
+               '{$this->post_data['mobile']}',
+               '{$this->post_data['gender']}',
+               '{$this->post_data['entry_type']}',
+               '{$this->post_data['entry_location']}',
+               '{$this->post_data['meals']}',
+               '{$this->post_data['accomodation']}',
+               '{$this->post_data['payment']}',
+               '{$this->post_data['newsletters']}',
+               now());
+EOD;
+    
+    }
+
+    private function insertUserRegistration() {
+       $res = $this->db->query($this->query_template);
+       if ($this->db->insert_id) {
+           return true;    
+       }
+
+       
+        
+    } 
+
 }
 
 
