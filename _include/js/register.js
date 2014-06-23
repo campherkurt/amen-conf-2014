798567a131a8657e457ecbfa79fae2519a0419a3
kurtcam <kurtc@liquorice.co.za>
2014-06-23 01:49:41 +0000

    Add js for register logic, error handling,

--- _include/js/register.js
+++ _include/js/register.js
@@ -1,4 +1,50 @@
+var ticketCopyManager = function(entryType) {
+        var ticketCopyBuilder;
+            copy = {
+                entry: {
+                    'ticket_name': "Entry Ticket- R200",
+                    'blurb': "Food and Accommodation not included."
+                },
+                half_entry: {
+                    'ticket_name': "Half Conference Ticket- R300",
+                    'blurb': "Includes stationery and cap."
+                },    
+                full_entry: {
+                    'ticket_name': "Full Conference Ticket- R500",
+                    'blurb': "Includes stationery, cap, bag and t-shirt."
+                    
+                },
+                sponsor: {
+                    'ticket_name': "Sponsor Ticket- R2000",
+                    'blurb': "Includes stationery, cap, bag and t-shirt."
+                }
+        };
+        $('#ticket_name').html(copy[entryType].ticket_name);
+        $('#ticket_blurb').html(copy[entryType].blurb);
+            
+}; 
+    
+
+
+
 $(document).ready(function(){
+
+    ticketCopyManager($.cookie('amenConf2014__ticketType'));
+    
+    $("#reg-form input[name='entry_type']").val($.cookie('amenConf2014__ticketType'));
+    var confLocation = $.cookie('amenConf2014__Location');
+    if (confLocation === "cape-town") {
+        $("#reg-form input[name='entry_location']").val('ct');
+    }
+    else if (confLocation === "port-elizabeth") {
+        $("#reg-form input[name='entry_location']").val('pe');
+            
+    }
+    else {
+        $("#reg-form input[name='entry_location']").val('not sure');
+    }
+
+
     $("#reg-submit").click(function(){
         $("#reg-form").trigger('submit'); 
         return false 
@@ -14,6 +60,7 @@ $(document).ready(function(){
             mobile: "required",
             gender: "required",
             payment: "required",
+            
         },
         messages: {
             full_name: "Please enter a valid name.",
@@ -25,8 +72,7 @@ $(document).ready(function(){
             
         },
         submitHandler: function(e) {
-            $("#reg-form input[name='entry_type']").val($.cookie('amenConf2014__ticketType'));
-            $("#reg-form input[name='entry_location']").val($.cookie('amenConf2014__Location'));
+
             var post_data = $('#reg-form').serialize();
             $.ajax({
                 url: '/ajax/register.php',
@@ -34,19 +80,21 @@ $(document).ready(function(){
                 data: post_data,
                 dataType: 'json',
                 success: function(data) { 
-                    console.log(data);
+                    if (data.success == false) {
+                        var errors = data.error_list;
+                        for (var item in errors) {
+                            $("#reg-form input[name='" + errors[item].field_name  + "']").addClass('error').after('<label for="' + errors[item].field_name  + '" class="error">' + errors[item].message  + '</label>')
+                        }
+                        $('html,body').animate({scrollTop: $("#reg-form input[name='" + errors[0].field_name + "']").offset().top - 50},'slow');
+                        // Add successful feedback
+                    }
                 },
                 error: function(x, e, r) {
-                    console.log(x);    
+                    //console.log(x);
                 }
             });
         },   
-    
-    
     } );    
-    
-    
-    
-})
+});
 
 
