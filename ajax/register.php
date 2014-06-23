5236c1cc803f16d869078b5e7bb7168b6735a07d
kurtcam <kurtc@liquorice.co.za>
2014-06-23 01:50:25 +0000

    Add proper defaults, add headers, 200

--- ajax/register.php
+++ ajax/register.php
@@ -10,23 +10,24 @@ if (isset($_POST['full_name'])) {
         array('field_name'=>'email',          'required'=>true,   'type'=>'email'),
         array('field_name'=>'mobile',         'required'=>true,   'type'=>'numeric'),
         array('field_name'=>'gender',         'required'=>true,   'type'=>'specific', 'defaults'=>array('m', 'f')),
-        array('field_name'=>'entry_type',     'required'=>true,   'type'=>'specific', 'defaults'=>array('entry', 'half-entry', 'full-entry', 'sponsor')),
-        array('field_name'=>'entry_location', 'required'=>true,   'type'=>'specific', 'defaults'=>array('pe', 'ct')),
-        array('field_name'=>'meals',          'required'=>false,  'type'=>'specific', 'defaults'=>array('sabbath', 'sunday', 'monday')),
+        array('field_name'=>'entry_type',     'required'=>true,   'type'=>'specific', 'defaults'=>array('entry', 'half_entry', 'full_entry', 'sponsor')),
+        array('field_name'=>'entry_location', 'required'=>true,   'type'=>'specific', 'defaults'=>array('pe', 'ct', 'not sure')),
+        array('field_name'=>'meals',          'required'=>false,  'type'=>'checkbox', 'defaults'=>array('sabbath', 'sunday', 'monday')),
         array('field_name'=>'accomodation',   'required'=>false,  'type'=>'specific', 'defaults'=>array('yes', 'no')),
         array('field_name'=>'payment',        'required'=>true,   'type'=>'specific', 'defaults'=>array('eft', 'deposit', 'cheque')),
-        array('field_name'=>'newsletter',     'required'=>false,  'type'=>'specific', 'defaults'=>array('monthly', 'conf_updates'))
+        array('field_name'=>'newsletter',     'required'=>false,  'type'=>'checkbox', 'defaults'=>array('monthly', 'conf_updates'))
     );
     $validate = new Validate($_POST, $fields);
 
     if ($validate->valid()) {
         $register = new Registration();
         $register->createRegistration();
-        echo json_encode(array('yay'=>'wotsup'));
+        echo json_encode(array('success'=>'true'));
+        exit();
     }
-    header("HTTP/1.1 400 AMEN say no no!!", TRUE, 400);
+    header("HTTP/1.1 200 AMEN say no no!!", TRUE, 200);
     echo json_encode($validate->get_error_messages());
-
+    exit();
     
 }
 ?>
